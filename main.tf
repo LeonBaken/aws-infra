f730834feeba040d2891dc8a26166e2b0901767a
Jun Liang Linux Subsystem <LiangJunAddress@gmail.com>
2023-03-08 09:13:16 -0500

    default encryption for bucket added

--- main.tf
+++ main.tf
@@ -182,3 +182,11 @@ resource "aws_s3_bucket_lifecycle_configuration" "s3_bucket_lifecycle_configurat
     status = "Enabled"
   }
 }
+resource "aws_s3_bucket_server_side_encryption_configuration" "bucket_encryption" {
+  bucket = aws_s3_bucket.s3_bucket.bucket
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
